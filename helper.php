2700f08bc7107832c658c6ce52a8f69353270c99
gdany91 <you@example.comdgarofalo52@gmail.com>
2023-01-11 18:44:46 +0100

    session non funzionante

--- helper.php
+++ helper.php
@@ -6,12 +6,13 @@ function generatePassword($length) {
 
     $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_[]{}<>~`+=,.;:/?|';
     $password = '';
+
     for ($i = 0; $i < $length; $i++) {
         $password .= $chars[rand(0, strlen($chars) - 1)];
     }
     return $password;
    
-    $_SESSION['mail'] = $mail;
+    $_SESSION['password'] = $password;
 
     header('location: pagePasw.php');
 
